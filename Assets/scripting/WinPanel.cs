68812e87e0ed8c884f46e5bb06afc2968d22e925
longTngPh61686 <longtngph61686@gmail.com>
2025-04-02 17:11:25 +0700

    Die Animation

---
+++ Assets/scripting/WinPanel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinPanel : MonoBehaviour
+{
+    private const string LEVEL_KEY = "LastCompletedLevel";
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void NextLevel()
+    {
+        int nextLevel = PlayerPrefs.GetInt(LEVEL_KEY, 1) + 1;
+        PlayerPrefs.SetInt(LEVEL_KEY, nextLevel);
+        SceneManager.LoadScene("Man" + nextLevel);
+    }
+}
